ebce580b36594f2b48f6d8d5c57f9f85fdfec2a5
僕話ヒノトリ <red5brack5@gmail.com>
2023-01-19 23:22:06 +0900

    battle+

---
+++ character_delete.php
@@ -0,0 +1,28 @@
+<?php
+require_once "./functions.php";
+$pdo = connect_vt_db();
+
+//受信確認用
+//echo('<pre>');
+//var_dump($_GET);
+//echo('</pre>');
+//exit();
+
+$id = $_GET['id'];
+
+
+
+$sql = 'DELETE FROM character_status WHERE id=:id';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':id', $id, PDO::PARAM_STR);
+
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+header("Location:vt_list.php");
+exit();
